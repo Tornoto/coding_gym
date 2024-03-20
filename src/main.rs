d53f42cd72953e4824e0e93b134b5005620a0f76
tornoto <LiuyuLite@163.com>
2024-03-21 07:58:56 +0800

    complete two_sum

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use coding_gym::problems::two_sum::*;
 fn main() {
-    get();
+    println!("Hello world!");
 }
--- src/problems/two_sum.rs
+++ src/problems/two_sum.rs
@@ -1,3 +1,22 @@
-pub fn get(){
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    for (idx, ele) in nums.iter().enumerate() {
+        let other = target - ele;
+        if let Some(other_idx) = map.get(&other) {
+            return vec![*other_idx as i32, idx as i32];
+        } else {
+            map.insert(*ele, idx as i32);
+        }
+    }
+    Vec::new()
+}
+
+#[test]
+
+fn test_two_sum() {
+    assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+    assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+    assert_eq!(two_sum(vec![3, 3], 6), vec![0, 1]);
 }
