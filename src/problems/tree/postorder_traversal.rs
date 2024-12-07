a3b265e7dd5bd6e81b0dd438b0417a63c4d02943
tornoto <LiuyuLite@163.com>
2024-12-07 20:57:00 +0800

    add comment

--- src/problems/tree/postorder_traversal.rs
+++ src/problems/tree/postorder_traversal.rs
@@ -5,7 +5,7 @@ use super::binary_tree::TreeNode;
 
 pub fn postorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
     let mut result: Vec<i32> = vec![];
-    postorder_iteration_deque(&root, &mut result);
+    post_iteraction_deque(&root, &mut result);
     result
 }
 
@@ -17,7 +17,15 @@ fn postorder_recursive(root: &Option<Rc<RefCell<TreeNode>>>, result: &mut Vec<i3
     }
 }
 
-fn postorder_iteration_deque(root: &Option<Rc<RefCell<TreeNode>>>, result: &mut Vec<i32>) {
+/// use two stacks to implement post order: a support stack and a result stack
+/// push the root into support stack first
+/// while support stack is not empty
+/// 1. pop a node out and push the node into result
+/// 2. push the left child of the node into support stack
+/// 3. push the right child of the node into support stack
+/// as a result, the result stack from buttom to top is root, [right child, left child ...]
+/// finally we pop elements in result stack out and get the post order result
+fn postorder_iteration_bideque(root: &Option<Rc<RefCell<TreeNode>>>, result: &mut Vec<i32>) {
     let mut stack_support = VecDeque::new();
     let mut stack_result = VecDeque::new();
 
@@ -36,6 +44,27 @@ fn postorder_iteration_deque(root: &Option<Rc<RefCell<TreeNode>>>, result: &mut 
     while !stack_result.is_empty() {
         result.push(stack_result.pop_front().unwrap().borrow().val);
     }
+}
+
+/// previous method uses two stacks. in fact, we can use the result list to act as the result stack
+/// before return the result list, we just need to reverse it
+fn post_iteraction_deque(root: &Option<Rc<RefCell<TreeNode>>>, result: &mut Vec<i32>) {
+    let mut stack = VecDeque::new();
+    if let Some(node) = root {
+        stack.push_back(node.clone());
+    }
+
+    while let Some(node) = stack.pop_back() {
+        if let Some(left) = &node.borrow().left {
+            stack.push_back(left.clone());
+        }
+        if let Some(right) = &node.borrow().right {
+            stack.push_back(right.clone());
+        }
+        result.push(node.borrow().val);
+    }
+
+    result.reverse();
 }
 
 #[cfg(test)]
