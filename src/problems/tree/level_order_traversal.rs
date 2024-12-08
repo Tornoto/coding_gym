08fda0bc0994e3ad9ee01933e625fe951a772ebe
tornoto <LiuyuLite@163.com>
2024-12-08 20:05:15 +0800

    add some problems based on level order traversal

--- src/problems/tree/level_order_traversal.rs
+++ src/problems/tree/level_order_traversal.rs
@@ -32,6 +32,54 @@ fn level_order_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
     result
 }
 
+/// https://leetcode.cn/problems/binary-tree-level-order-traversal-ii/description/
+fn level_order_traversal_bottom(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+    let mut result = level_order_traversal(root);
+    result.reverse();
+    result
+}
+
+/// https://leetcode.cn/problems/binary-tree-right-side-view/description/
+/// push the last element of each level into result
+pub fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut result = vec![];
+    let mut stack = vec![];
+    let mut stack_support = vec![];
+    if let Some(node) = root {
+        stack.push(node);
+    }
+
+    while !stack.is_empty() {
+        let mut level_result = vec![];
+        while let Some(node) = stack.pop() {
+            level_result.push(node.borrow().val);
+            if let Some(left) = node.borrow().left.clone() {
+                stack_support.push(left);
+            }
+            if let Some(right) = node.borrow().right.clone() {
+                stack_support.push(right);
+            }
+        }
+        while let Some(node) = stack_support.pop() {
+            stack.push(node);
+        }
+        if let Some(val) = level_result.last() {
+            result.push(*val);
+        }
+    }
+
+    result
+}
+
+/// https://leetcode.cn/problems/average-of-levels-in-binary-tree/description/
+pub fn average_of_levels(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<f64> {
+    let result = level_order_traversal(root);
+    result
+        .into_iter()
+        .map(|arr: Vec<i32>| (arr.iter().sum::<i32>()) as f64 / arr.len() as f64)
+        .collect()
+}
+
 /// same solution with different result type
 fn level_order_traversal2(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
     let mut result = vec![];
@@ -69,6 +117,20 @@ mod test {
     fn test_level_order_traversal() {
         let root = get_test_case();
         let result = level_order_traversal(root);
+        println!("res: {:?}", result);
+    }
+
+    #[test]
+    fn test_right_side_view() {
+        let root = get_test_case();
+        let result = right_side_view(root);
+        println!("res: {:?}", result);
+    }
+
+    #[test]
+    fn test_average_of_levels() {
+        let root = get_test_case();
+        let result = average_of_levels(root);
         println!("res: {:?}", result);
     }
 }
