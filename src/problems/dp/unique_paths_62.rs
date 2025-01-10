75a52d753d18d7b5a13bc512bff97821082c98d6
tornoto <LiuyuLite@163.com>
2025-01-10 19:33:31 +0800

    add unique paths v3 by calculating combination

--- src/problems/dp/unique_paths_62.rs
+++ src/problems/dp/unique_paths_62.rs
@@ -35,3 +35,29 @@ pub fn unique_paths_v2(m: i32, n: i32) -> i32 {
 
     paths[n - 1]
 }
+
+pub fn unique_paths_v3(m: i32, n: i32) -> i32 {
+    let steps = m + n - 2;
+    combination(steps, m - 1)
+}
+
+fn combination(m: i32, n: i32) -> i32 {
+    if n == 0 || n == m {
+        return 1;
+    }
+    if n > m {
+        return 0;
+    }
+
+    let n = n.min(m - n);
+    let mut comb = 1;
+    for i in 0..n {
+        if comb % (i + 1) == 0 {
+            comb = comb / (i + 1) * (m - i);
+        } else {
+            comb = comb * (m - i) / (i + 1);
+        }
+    }
+
+    comb
+}
