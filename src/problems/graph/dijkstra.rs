09746b3c3f503e27f7458cd40019b24bf826b24e
tornoto <LiuyuLite@163.com>
2025-02-24 20:43:08 +0800

    add dijkstra using binary heap

--- src/problems/graph/dijkstra.rs
+++ src/problems/graph/dijkstra.rs
@@ -1,3 +1,8 @@
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashMap},
+};
+
 #[allow(unused)]
 
 /// Dijkstra's algorithm using adjacency matrix representation
@@ -47,6 +52,47 @@ fn dijkstra(graph: &Vec<Vec<i32>>, from: usize, to: usize) -> i32 {
     dist[to]
 }
 
+#[derive(Debug)]
+struct Edge {
+    to: usize,
+    cost: i32,
+}
+
+// 图结构
+// key  value
+// 1    [2, 3]
+// 2    [3, 5]
+fn dijkstra_heap(graph: &HashMap<usize, Vec<Edge>>, start: usize) -> HashMap<usize, i32> {
+    let mut dists = HashMap::new();
+    let mut heap = BinaryHeap::new();
+
+    dists.insert(start, 0);
+    heap.push((Reverse(0), start));
+
+    while let Some((Reverse(cost), node)) = heap.pop() {
+        // 如果当前节点的距离已经大于记录的最短距离，则跳过
+        if cost > *dists.get(&node).unwrap_or(&i32::MAX) {
+            continue;
+        }
+
+        // 遍历当前节点的邻居
+        if let Some(edges) = graph.get(&node) {
+            for edge in edges {
+                let next_node = edge.to;
+                let new_cost = cost + edge.cost;
+
+                // 如果找到更短的路径，则更新距离并加入堆中
+                if new_cost < *dists.get(&next_node).unwrap_or(&i32::MAX) {
+                    dists.insert(next_node, new_cost);
+                    heap.push((Reverse(new_cost), next_node));
+                }
+            }
+        }
+    }
+
+    dists
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -64,5 +110,27 @@ mod test {
         let to = 4;
         let dist = dijkstra(&graph, from, to);
         assert_eq!(dist, 7);
+    }
+
+    #[test]
+    fn test_heap() {
+        // 构建图的邻接表表示
+        let mut graph = HashMap::new();
+        graph.insert(0, vec![Edge { to: 1, cost: 4 }, Edge { to: 2, cost: 1 }]);
+        graph.insert(1, vec![Edge { to: 3, cost: 1 }]);
+        graph.insert(2, vec![Edge { to: 1, cost: 2 }, Edge { to: 3, cost: 5 }]);
+        graph.insert(3, vec![]);
+
+        // 起点
+        let start = 0;
+
+        // 计算最短路径
+        let shortest_distances = dijkstra_heap(&graph, start);
+
+        // 输出结果
+        println!("从节点 {} 出发的最短路径：", start);
+        for (node, distance) in &shortest_distances {
+            println!("到节点 {}: 距离 {}", node, distance);
+        }
     }
 }
