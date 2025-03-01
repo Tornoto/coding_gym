f5adb54a2334bb7c39b62b0b4bddc7ce8483c001
tornoto <LiuyuLite@163.com>
2025-03-01 22:19:46 +0800

    add some comments

--- src/problems/misc/move_zeroes_283.rs
+++ src/problems/misc/move_zeroes_283.rs
@@ -3,6 +3,7 @@ pub fn move_zeroes(nums: &mut Vec<i32>) {
     let n = nums.len();
     let mut left = 0;
 
+    // 找到第一个0
     while left < n && nums[left] != 0 {
         left += 1;
     }
@@ -10,10 +11,11 @@ pub fn move_zeroes(nums: &mut Vec<i32>) {
     let mut right = left;
 
     while left < n && right < n {
+        // 找到0之后第一个非0，考虑有连续0的情形
         while right < n && nums[right] == 0 {
             right += 1;
         }
-
+        // 交换，并同时移动指针
         while right < n && nums[right] != 0 {
             nums.swap(left, right);
             left += 1;
