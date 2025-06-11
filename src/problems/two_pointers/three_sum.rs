375cf74dc836c016d50fe927ce55c40e7a0029bc
tornoto <LiuyuLite@163.com>
2025-06-11 22:18:05 +0800

    write three sum agiain

--- src/problems/two_pointers/three_sum.rs
+++ src/problems/two_pointers/three_sum.rs
@@ -53,6 +53,54 @@ pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
     res
 }
 
+pub fn three_sum2(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    if nums.len() < 3 {
+        return vec![];
+    }
+    let mut nums = nums;
+    nums.sort_unstable();
+
+    let mut start = 0;
+
+    let mut result = vec![];
+    while start < nums.len() - 2 {
+        // 提前退出
+        if nums[start] > 0 {
+            return result;
+        }
+        let mut left = start + 1;
+        let mut right = nums.len() - 1;
+        while left < right {
+            let sum = nums[start] + nums[left] + nums[right];
+            if sum == 0 {
+                result.push(vec![nums[start], nums[left], nums[right]]);
+
+                while right > 1 && nums[right - 1] == nums[right] {
+                    right -= 1;
+                }
+                right -= 1;
+
+                while left < nums.len() - 1 && nums[left + 1] == nums[left] {
+                    left += 1;
+                }
+                left += 1;
+            } else if sum > 0 {
+                // 没必要跳过重复的元素。因为即使不跳过，下次循环进来发现sum>0,也会继续移动
+                // 这和提前移动去除重复元素的效率是一样的
+                right -= 1;
+            } else {
+                left += 1;
+            }
+        }
+        // the start pointer moves right and skips elements of the same value
+        while start < nums.len() - 2 && nums[start + 1] == nums[start] {
+            start += 1;
+        }
+        start += 1;
+    }
+
+    result
+}
 #[cfg(test)]
 mod test {
     use super::*;
@@ -60,19 +108,19 @@ mod test {
     #[test]
     fn test_three_sum() {
         let nums = vec![-1, 0, 1, 2, -1, -4];
-        let res = three_sum(nums);
+        let res = three_sum2(nums);
         println!("{:?}", res);
         // //
         let nums = vec![0, 1, 1];
-        let res = three_sum(nums);
+        let res = three_sum2(nums);
         println!("{:?}", res);
 
         let nums = vec![0, 0, 0];
-        let res = three_sum(nums);
+        let res = three_sum2(nums);
         println!("{:?}", res);
 
         let nums = vec![0, 0, 0, 0];
-        let res = three_sum(nums);
+        let res = three_sum2(nums);
         println!("{:?}", res);
     }
 }
